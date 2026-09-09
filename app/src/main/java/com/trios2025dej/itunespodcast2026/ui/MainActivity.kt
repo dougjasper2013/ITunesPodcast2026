5c06f2b8ad7bf0347503fa0700b5a1f5c4e9a67c
Douglas Jasper <doug.jasper@trios.com>
2026-09-09 08:59:52 -0400

    Added MainActivity imports

--- app/src/main/java/com/trios2025dej/itunespodcast2026/ui/MainActivity.kt
+++ app/src/main/java/com/trios2025dej/itunespodcast2026/ui/MainActivity.kt
@@ -6,6 +6,14 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.trios2025dej.itunespodcast2026.R
+import android.widget.Button
+import android.widget.EditText
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
