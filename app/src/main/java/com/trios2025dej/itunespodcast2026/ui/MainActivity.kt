f900d7dda4ee521e556810ac3e90f43c5a89cc7d
Douglas Jasper <doug.jasper@trios.com>
2026-09-09 10:23:48 -0400

    Displaying First Item in List

--- app/src/main/java/com/trios2025dej/itunespodcast2026/ui/MainActivity.kt
+++ app/src/main/java/com/trios2025dej/itunespodcast2026/ui/MainActivity.kt
@@ -11,19 +11,71 @@ import android.widget.EditText
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.trios2025dej.itunespodcast2026.data.ITunesApi
+import com.trios2025dej.itunespodcast2026.data.Podcast
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import kotlinx.coroutines.launch
+import retrofit2.create
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var editTextSearch: EditText
+    private lateinit var buttonSearch: Button
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: PodcastAdapter
+    private lateinit var api: ITunesApi
+    // private var podcastList: MutableList<Podcast> = mutableListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        // Init Views
+        editTextSearch = findViewById(R.id.editTextSearch)
+        buttonSearch = findViewById(R.id.buttonSearch)
+        recyclerView = findViewById(R.id.recyclerView)
+
+        // Setup RecyclerView
+        adapter = PodcastAdapter(emptyList())
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
+
+        // Init Retrofit
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://itunes.apple.com")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        api = retrofit.create(ITunesApi::class.java)
+
+        buttonSearch.setOnClickListener {
+            val query = editTextSearch.text.toString().trim()
+
+            if (query.isNotEmpty()) {
+                searchPodcasts(query)
+            }
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
+
+    private fun searchPodcasts(query: String) {
+        lifecycleScope.launch {
+            try {
+                val response = api.searchPodcasts(query)
+                // podcastList.clear()
+                // podcastList.addAll(response.results)
+                adapter.updateList(response.results)
+            }
+            catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
 }
