022f9bc72b51db39fbd1b1cb48989dac2b2183c8
Douglas Jasper <doug.jasper@trios.com>
2026-09-14 09:22:06 -0400

    Added PodcastDetailActivity.kt

---
+++ app/src/main/java/com/trios2025dej/itunespodcast2026/ui/PodcastDetailActivity.kt
@@ -0,0 +1,21 @@
+package com.trios2025dej.itunespodcast2026.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.trios2025dej.itunespodcast2026.R
+
+class PodcastDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_podcast_detail)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
