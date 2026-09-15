14b9e6d4d34ef60401c06a567d7460c4733ca30f
Douglas Jasper <doug.jasper@trios.com>
2026-09-15 08:32:10 -0400

    Added AppDatabase.kt

---
+++ app/src/main/java/com/trios2025dej/itunespodcast2026/data/AppDatabase.kt
@@ -0,0 +1,42 @@
+package com.trios2025dej.itunespodcast2026.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [SubscribedPodcast::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun subscriptionDao(): SubscriptionDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(
+            context: Context
+        ): AppDatabase {
+
+            return INSTANCE
+                ?: synchronized(this) {
+
+                    val instance =
+                        Room.databaseBuilder(
+                            context.applicationContext,
+                            AppDatabase::class.java,
+                            "podcast_database"
+                        ).build()
+
+                    INSTANCE = instance
+
+                    instance
+                }
+        }
+    }
+}
