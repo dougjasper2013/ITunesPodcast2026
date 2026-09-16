f456d6104c3abae91d3e1ac3dbc05ff2ea1ca2c0
Douglas Jasper <doug.jasper@trios.com>
2026-09-16 08:23:49 -0400

    Updated Episode.kt

--- app/src/main/java/com/trios2025dej/itunespodcast2026/data/Episode.kt
+++ app/src/main/java/com/trios2025dej/itunespodcast2026/data/Episode.kt
@@ -1,8 +1,11 @@
 package com.trios2025dej.itunespodcast2026.data
 
-data class Episode(val title: String,
-                   val description: String,
-                   val pubDate: String,
-                   val mediaUrl: String,
-                   val mediaType: String,
-                   val isVideo: Boolean)
+data class Episode(
+    val guid: String,
+    val title: String,
+    val description: String,
+    val pubDate: String,
+    val mediaUrl: String,
+    val mediaType: String,
+    val isVideo: Boolean
+)
